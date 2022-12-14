e1806d59b8bf8dcd9794cf16f1ab1e25ad07454e
yemyintsoe <yms.yemyintsoe@gmail.com>
2022-12-14 23:23:29 +0630

    yms

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -22,7 +22,49 @@ class RoleSeeder extends Seeder
             'role' => 'Editor'
         ]);
         Role::create([
-            'role' => 'Admin'
+            'role' => 'Admin',
+            'permissions' => json_encode([
+                [
+                    "menu" => "Dashboard",
+                    "read" =>  true,
+                    "write" =>  true,
+                    "update" =>  true,
+                    "delete" => true,
+                    "name" => "dashboard"
+                ],
+                [
+                    "menu" => 'Tag',
+                    "read" => false,
+                    "write" => false,
+                    "update" => false,
+                    "delete" => false,
+                    "name" => 'tags'
+                ],
+                [
+                    "menu" => 'Category',
+                    "read" => false,
+                    "write" => false,
+                    "update" => false,
+                    "delete" => false,
+                    "name" => 'categories'
+                ],
+                [
+                    "menu" => 'User',
+                    "read" => true,
+                    "write" => true,
+                    "update" => true,
+                    "delete" => true,
+                    "name" => 'users'
+                ],
+                [
+                    "menu" => 'Role',
+                    "read" => true,
+                    "write" => true,
+                    "update" => true,
+                    "delete" => true,
+                    "name" => 'roles'
+                ]
+            ])
         ]);
     }
 }
